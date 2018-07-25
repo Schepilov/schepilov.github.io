15e594a2922fd289740ee3cafc3621b829b0bc67
Alex Schepilov <globuz2010@gmail.com>
2018-07-25 17:33:59 +0300

    bfa two

---
+++ bfa/static/js/2.8b025451.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{49:function(e,t,n){"use strict";function u(){return{translation:{}}}Object.defineProperty(t,"__esModule",{value:!0}),t.default=u}});
+//# sourceMappingURL=2.8b025451.chunk.js.map
